c6ad55587802c8c5aa4a998179730205e17d4e2d
shallah1999 <ahmmaa.4576@gmail.com>
2023-03-13 16:00:02 +0200

    ahmed

--- src/pages/balance/index.tsx
+++ src/pages/balance/index.tsx
@@ -3,11 +3,10 @@ import { GeneralLayout } from "layouts";
 
 export const Balance = () => {
   return (
-     
       <GeneralLayout>
         <Card>sdawdwd</Card>
       </GeneralLayout>
- 
+
   );
 };
 export default Balance;
