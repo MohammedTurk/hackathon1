cfb55f23cfb8e8d0df131049f4e8fb8c347d5339
mohammedturk <mohammadjturk7@gmail.com>
2023-03-13 13:56:57 +0200

    'first'

---
+++ src/lib/@heroicons/index.ts
@@ -0,0 +1,25 @@
+import {
+  ExclamationTriangleIcon as ErrorIconMini,
+  CheckCircleIcon as CheckCircleIconMini,
+  EyeIcon as EyeIconMini,
+  EyeSlashIcon as EyeSlashIconMini,
+  ChevronLeftIcon as ChevronLeftIconMini,
+  ArrowUpTrayIcon as ArrowUpTrayIconMini,
+  XMarkIcon as XMarkIconMini,
+  InformationCircleIcon as InformationCircleIconMini,
+  CheckIcon as CheckIconMini,
+} from "@heroicons/react/20/solid";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+
+export {
+  ErrorIconMini,
+  CheckCircleIconMini,
+  EyeIconMini,
+  EyeSlashIconMini,
+  ChevronLeftIconMini,
+  ArrowUpTrayIconMini,
+  XMarkIconMini,
+  InformationCircleIconMini,
+  InformationCircleIcon,
+  CheckIconMini,
+};
