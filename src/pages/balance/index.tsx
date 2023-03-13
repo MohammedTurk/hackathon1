88d8bc88dd2e96393adbda8ea575e4c8002ecc29
Mahmouddaw2002 <mahmouddaw1347@gmail.com>
2023-03-14 00:35:44 +0200

    newedit3

--- src/pages/balance/index.tsx
+++ src/pages/balance/index.tsx
@@ -1,17 +1,11 @@
-import { AddReciepient } from "features/payout/components";
-import { TransferPreview } from "features/payout/components";
-import { Delete } from "features/payout/components";
-import { Recipients } from "features/payout/components";
+
 import { Card, BalanceCard } from "components";
 import { GeneralLayout } from "layouts";
 
 export const Balance = () => {
   return (
     <GeneralLayout rightSide={<BalanceCard />}>
-      {/* <AddReciepient /> */}
-      {/* <TransferPreview /> */}
-      {/* <Delete /> */}
-      {/* <Recipients /> */}
+
     </GeneralLayout>
   );
 };
